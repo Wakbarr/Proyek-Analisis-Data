0f0e286030154f44b22d63d77478b47e1b74b9ea
Akbar Widianto <wdntoakbar@gmail.com>
2025-02-20 11:02:50 +0700

    fix: function

--- Dashboardd/function.py
+++ Dashboardd/function.py
@@ -4,11 +4,8 @@ class AnalyticsTool:
 
     def create_sum_order_items_df(self):
         sum_order_items_df = self.df.groupby("product_category_name_english")["order_item_id"].count().reset_index()
-        sum_order_items_df.rename(columns={
-            "order_item_id": "order_item_count"
-        }, inplace=True)
+        sum_order_items_df.rename(columns={"order_item_id": "order_item_count"}, inplace=True)
         sum_order_items_df = sum_order_items_df.sort_values(by='order_item_count', ascending=False)
-
         return sum_order_items_df
 
     def create_monthly_performance_df(self):
@@ -23,7 +20,6 @@ class AnalyticsTool:
             "payment_value": "total_revenue"
         }, inplace=True)
         recent_months_performance = monthly_performance.tail(8)
-
         return recent_months_performance
     
     def create_daily_order_df(self):
@@ -36,7 +32,6 @@ class AnalyticsTool:
             "order_id": "order_count",
             "payment_value": "total_revenue"
         }, inplace=True)
-        
         return daily_order_df
     
     def create_sum_spend_df(self):
@@ -47,17 +42,13 @@ class AnalyticsTool:
         sum_spend_df.rename(columns={
             "payment_value": "total_spend"
         }, inplace=True)
-
         return sum_spend_df
       
     def create_city_df(self):
         bycity_df = self.df.groupby("customer_city")['customer_id'].nunique().reset_index()
-        bycity_df.rename(columns={
-            "customer_id": "customer_count"
-        }, inplace=True)
+        bycity_df.rename(columns={"customer_id": "customer_count"}, inplace=True)
         most_city = bycity_df.loc[bycity_df['customer_count'].idxmax(), 'customer_city']
         bycity_df = bycity_df.sort_values(by='customer_count', ascending=False)
-
         return bycity_df, most_city
 
     def create_rfm_df(self):
@@ -71,20 +62,30 @@ class AnalyticsTool:
         recent_date = self.df["order_purchase_timestamp"].dt.date.max()
         rfm_df["recency"] = rfm_df["max_order_timestamp"].apply(lambda x: (recent_date - x).days)
         rfm_df.drop("max_order_timestamp", axis=1, inplace=True)
-    
         return rfm_df
-    
+import plotly.express as px
+
 class BrazilGeospatial:
-    def __init__(self, data, plt, mpimg, urllib, st):
+    def __init__(self, data, st):
         self.data = data
-        self.plt = plt
-        self.mpimg = mpimg
-        self.urllib = urllib
         self.st = st
 
     def plot(self):
-        brazil = self.mpimg.imread(self.urllib.request.urlopen('https://i.pinimg.com/originals/3a/0c/e1/3a0ce18b3c842748c255bc0aa445ad41.jpg'),'jpg')
-        ax = self.data.plot(kind="scatter", x="geolocation_lng", y="geolocation_lat", figsize=(10,10), alpha=0.3,s=0.3,c='green')
-        self.plt.axis('off')
-        self.plt.imshow(brazil, extent=[-73.98283055, -33.8,-33.75116944,5.4])
-        self.st.pyplot()
+        if self.data.empty:
+            self.st.write("No geospatial data available.")
+            return
+        # Membuat peta interaktif dengan Plotly Express
+        fig = px.scatter_mapbox(
+            self.data,
+            lat="geolocation_lat",
+            lon="geolocation_lng",
+            hover_name="customer_unique_id",
+            zoom=3,
+            height=600,
+            width=800
+        )
+        fig.update_layout(
+            mapbox_style="open-street-map",
+            margin={"r": 0, "t": 0, "l": 0, "b": 0}
+        )
+        self.st.plotly_chart(fig, use_container_width=True)
